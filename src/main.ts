554c0f686f7ec968f2079f170f257849a8d8c5f1
Evert De Spiegeleer <evert.de.spiegeleer@gmail.com>
2025-05-17 15:09:20 +0200

    refactor: ♻️ minor changes

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import "./wasm_exec.js";
 import path from "node:path";
 import { InvalidTemplateError, TemplatingError } from "./errors.ts";
 
+const go = new Go();
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = new URL(__filename, import.meta.url).pathname;
+
 export const parse = async (template: string, values: any) => {
-    const go = new Go();
-    const __filename = new URL(import.meta.url).pathname;
-    const __dirname = new URL(__filename, import.meta.url).pathname;
     const wasmBuffer = await fs.readFile(path.join(__dirname, "../main.wasm"));
     const { instance } = await WebAssembly.instantiate(wasmBuffer, go.importObject);
     go.run(instance);
     const result = (globalThis as any).Render(template, JSON.stringify(values)) as string;
+    go.exit(0);
 
     if (result.startsWith('Template exec error:')) {
         throw new TemplatingError(result.match(/Template exec error:.+?executing "tpl".+?: (.+)/)?.[1])
@@ -24,7 +26,6 @@ export const parse = async (template: string, values: any) => {
         throw new Error(result);
     }
 
-    go.exit(0);
     return result
 }
 
